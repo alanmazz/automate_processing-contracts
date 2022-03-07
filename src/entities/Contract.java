7d496ec69b0cfc0700680b1939593b0dbca393b5
Alan Mazzolenis <alangalvaogg@gmail.com>
2022-03-07 09:40:47 -0300

    Contract created

---
+++ src/entities/Contract.java
@@ -0,0 +1,57 @@
+package entities;
+
+import java.util.Date;
+
+public class Contract {
+	
+	private Integer number;
+	private Date date;
+	private Double totalValue;
+	
+	private Installment installment;
+	
+	public Contract() {
+	}
+
+	public Contract(Integer number, Date date, Double totalValue, Installment installment) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+		this.installment = installment;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public Installment getInstallment() {
+		return installment;
+	}
+
+	public void setInstallment(Installment installment) {
+		this.installment = installment;
+	}
+	
+	
+
+}
---
+++ src/entities/Installment.java
@@ -0,0 +1,36 @@
+package entities;
+
+import java.util.Date;
+
+public class Installment {
+	
+	protected Date dueDate;
+	protected Double amount;
+	
+	public Installment() {
+	}
+
+	public Installment(Date dueDate, Double amount) {
+		this.dueDate = dueDate;
+		this.amount = amount;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	
+
+}
